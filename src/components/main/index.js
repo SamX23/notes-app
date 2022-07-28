f4d94ea816a7eb8a9b077d598238f0803d6a4091
SamX23 <skalexsong@gmail.com>
2022-07-29 00:34:49 +0700

    Update : add toast notification

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -2,11 +2,24 @@ import { useState } from "react";
 import getInitialData from "../../utils/data";
 import CreateNotes from "../createNotes";
 import Notes from "../Notes";
+import { toast } from "react-toastify";
 import "./style.css";
 
 const Main = () => {
   const [data, setData] = useState(getInitialData());
-  const onDelete = (id) => setData(data.filter((note) => note.id !== id));
+  const onDelete = (id) => {
+    setData(data.filter((note) => note.id !== id));
+
+    toast.success("Notes deleted!", {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+    });
+  };
 
   return (
     <main>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <App />
+
+    {/* Third Party */}
+    <ToastContainer
+      position="bottom-right"
+      autoClose={3000}
+      hideProgressBar
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover={false}
+    />
   </React.StrictMode>
 );
